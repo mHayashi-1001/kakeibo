a95a5294e2d9f51426a04038200f7acc8471a6c2
mHayashi-1001 <rena_sun_9on@yahoo.co.jp>
2025-07-06 16:27:21 +0000

    Revert "不要記載削除"
    This reverts commit d0392da676d553ed995a32a30aff8c23bd686659.

--- project/next.config.ts
+++ project/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      buffer: require.resolve("buffer"),
+      async_hooks: require.resolve("async_hooks"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
