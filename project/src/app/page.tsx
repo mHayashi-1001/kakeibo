f4650bccdef1bb1d5e7994fcfaf8aa81d5ecb4d7
mHayashi-1001 <rena_sun_9on@yahoo.co.jp>
2025-07-14 14:56:37 +0000

    現時点のファイル整理

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -12,6 +12,14 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <a
+          className="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600"
+          href="/entry"
+        >
+          Entry
+        </a>
+
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
